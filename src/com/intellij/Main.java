dbb4713c6c72d52dcc201df122b53b5ab7831a55
Ian F <ian.fiddeman@gmail.com>
2021-10-18 14:47:58 +0100

    comparing objects

--- src/com/intellij/Main.java
+++ src/com/intellij/Main.java
@@ -5,19 +5,12 @@ package com.intellij;
 public class Main {
 
     public static void main(String[] args) {
-        var control = new UIControl(true);
-        var textBox = new TextBox();
-        show(control);
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
 
     }
 
-    private static void show(UIControl control) {
-        if (control instanceof TextBox) {
-            var textBox = (TextBox)control;
-            textBox.setText("Hello ");
-        }
 
-
-        System.out.println(control);
-    }
 }
